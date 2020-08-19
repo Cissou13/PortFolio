57ab970eda95c25037f8db3281d5415c3dabb070
Cissou13 <cyrille.fabre@gmail.com>
2020-08-19 21:25:28 +0200

    1er commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function a2020() {
+  document.write('Conseiller de clientèle, Société Générale');
+}
+function a2019() {
+  document.write('Gestionnaire de courses, Taxis Radio Marseille ');
+}
+function a2017() {
+  document.write(
+    'Gestionnaire Expérimenté mutuelles (14Mutuelles), iGestion groupe CEGEDIM '
+  );
+}
+function a2014() {
+  document.write('Technicien Hotline niv 1, Dartybox ');
+}
+function a2013() {
+  document.write('Conseiller multimédia, Certicall(Free) ');
+}
